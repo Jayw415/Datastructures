ef47bfedf61c16b428cd076bcce0916bbe65da1f
Jayw415 <113055188+Jayw415@users.noreply.github.com>
2026-03-30 22:59:11 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,87 +1,40 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class Main {
-  
-   
+class Main {
   public static void main(String[] args) {
-      System.out.println("Unsorted Array ---------------------------------------------------");
-      ArrayList<Integer> integerList = Lab4.getList();
-      Lab4.outputList(integerList);
-      System.out.println("\n\nBubble sort results ----------------------------------------------");
-      ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(integerList);
-      Lab4.outputList(bubbleSortedList);
 
-      System.out.println("\n\nInsertion sort results -------------------------------------------");
-      ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);  
-      Lab4.outputList(insertionSortedList);
-      
-    }
-}
-
-class Lab4 {
-
-  public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) 
-  
-  {
-long startTime = System.nanoTime();
-    for(int o = 1; o < integerList.size(); o++)
-{
-  int currentElement = integerList.get(o);
-  int f;
-  for(f = o - 1; f >= 0 && integerList.get(f) > currentElement; f--)
-{
-   integerList.set(f + 1, f);
-   integerList.set(f, currentElement);
-}
-}
-   long endTime = System.nanoTime();
-long elapsedTime = endTime - startTime;
-  System.out.println(elapsedTime);
-return integerList;
-  }
-
-  public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) 
-  {
-    // Step 2 - Implement the bubble sort algorithm here
-long startTime = System.nanoTime();
-    boolean nextPass = true;
-for(int i = 1; i < integerList.size() && nextPass; i++)
-  {
-nextPass = false;
-for(int j = 0; j < integerList.size() - i; j++){
-  if(integerList.get(j) > integerList.get(j + 1))
-  {
-    int temp = integerList.get(j);
-    integerList.set(j, j + 1);
-    integerList.set(j + 1, temp);
-    nextPass = true;
-  }
-}
-}
-long endTime = System.nanoTime();
-long elapsedTime = endTime - startTime;
-  System.out.println(elapsedTime);
- return integerList;
-  }
-  public static ArrayList<Integer> getList() {
-    ArrayList<Integer> integerList = new ArrayList<>();
-    String line;
-    try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
-        while ((line = br.readLine()) != null) {
-            integerList.add(Integer.parseInt(line));
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    return integerList;
-  }
-
-  public static void outputList(ArrayList<Integer> integerList) {
-    for (int i = 0; i < integerList.size(); i++) {
-        System.out.print(integerList.get(i) + " ");
-    }
+    // Step 1 - Create a BST tree object called lab5Tree
+BST lab5tree = new BST();
+    // Step 2 - Insert the following values: 13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10
+lab5tree.insert(13);
+lab5tree.insert(22);
+lab5tree.insert(36);
+lab5tree.insert(5);
+lab5tree.insert(48);
+lab5tree.insert(17);
+lab5tree.insert(39);
+lab5tree.insert(2);
+lab5tree.insert(26);
+lab5tree.insert(40);
+lab5tree.insert(29);
+lab5tree.insert(34);
+lab5tree.insert(10);
+    // Step 3 - Delete the value 17
+lab5tree.delete(17);
+    // Step 4 - Traverse and output the values using inorder (sorted)
+System.out.println("\nIn Order: ");
+    lab5tree.inorder();
+    // Step 5 - Traverse and output the values using postorder
+System.out.println("\nPost Order: ");
+    lab5tree.postorder();
+    // Step 6 - Traverse and output the values using preorder
+System.out.println("\nPre Order: ");
+    lab5tree.preorder();
+    // Step 7 - Display the result of a search for the value 36
+System.out.println("Is there 36? " + lab5tree.search(36));
+    // Step 8 - Display the result of a search for the value 37
+System.out.println("Is there 37? " + lab5tree.search(37));
+    // Step 9 - Using the path() method, display the path from the root to 2
+System.out.println("The path to 2 " + lab5tree.path(2));
+    // Step 10 - Display the path from the root to 34
+System.out.println("The path to 34 " + lab5tree.path(34));
   }
 }
